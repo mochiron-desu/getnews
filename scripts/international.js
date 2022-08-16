34aaa256ccfd7c5542c67725fa20c445accfe6d1
Suraj S <qwerky2003@gmail.com>
2022-08-16 22:45:19 +0530

    Final Commit
    meow

---
+++ scripts/international.js
@@ -0,0 +1,21 @@
+apiKey = `42d166c6121c4770914a02777f34b8db`;
+const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+fetch(url1).then((res) => {
+    return res.json()
+})
+    .then((data) => {
+        data.articles.forEach(article => {
+            if (article.title == null || article.author == null || article.description == null || article.urlToImage == null) {
+                return
+            }
+            document.querySelector(".news1").innerHTML +=
+                `
+        <div class="news-box">
+            <div class="title"><a target="_blank" href=${article.url}>${article.title}</a></div><br>
+            <div class="author">${article.author}</div><br>
+            <div class="desc">${article.description}</div>
+            <img class="news-image" src=${article.urlToImage}>
+        </div>
+        `
+        })
+    })
---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+apiKey = `42d166c6121c4770914a02777f34b8db`;
+const url1 = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`
+const url2 = `
+https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=${apiKey}`
+fetch(url1).then((res) => {
+    return res.json()
+})
+    .then((data) => {
+        data.articles.forEach(article => {
+            if (article.title == null || article.author == null || article.description == null || article.urlToImage == null) {
+                return
+            }
+            document.querySelector(".news1").innerHTML +=
+                `
+        <div class="news-box">
+            <div class="title"><a target="_blank" href=${article.url}>${article.title}</a></div><br>
+            <div class="author">${article.author}</div><br>
+            <div class="desc">${article.description}</div>
+            <img class="news-image" src=${article.urlToImage}>
+        </div>
+        `
+        })
+    })
+
+fetch(url2).then((res)=>{
+    return res.json()
+}).then((data)=>{
+    console.log(data)
+    data.articles.forEach(article=>{
+        if (article.title == null || article.author == null || article.description == null || article.urlToImage == null) {
+            return
+        }
+        document.querySelector(".news2").innerHTML+=
+        `
+        <div class="news-box">
+            <div class="title"><a target="_blank" href=${article.url}>${article.title}</a></div><br>
+            <div class="author">${article.author}</div><br>
+            <div class="desc">${article.description}</div>
+            <img class="news-image" src=${article.urlToImage}>
+        </div>
+        `
+    })
+})
+
